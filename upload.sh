2eca31b8834f5da35e8dae85da50422201709f75
Niels Reijers <nielsreijers@gmail.com>
2023-10-18 19:01:57 +0200

    Add script to upload to Gandi

---
+++ upload.sh
@@ -0,0 +1,2 @@
+echo "put full-version/NielsReijers-full-version.pdf /lamp0/web/vhosts/www.nielsreijers.com/htdocs/user/pages/02.cv
+put single-page-version/NielsReijers-single-page.pdf /lamp0/web/vhosts/www.nielsreijers.com/htdocs/user/pages/02.cv" | sftp gandi
