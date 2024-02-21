b32ad98c6d3270a083048dea47fd09e343588081
Niels Reijers <nielsreijers@gmail.com>
2024-02-21 08:55:54 +0800

    Updated upload.sh

--- upload.sh
+++ upload.sh
@@ -1,2 +1,3 @@
 echo "put full-version/NielsReijers-full-version.pdf /lamp0/web/vhosts/www.nielsreijers.com/htdocs/user/pages/02.cv
-put single-page-version/NielsReijers-single-page.pdf /lamp0/web/vhosts/www.nielsreijers.com/htdocs/user/pages/02.cv" | sftp gandi
+put single-page-version/NielsReijers-single-page.pdf /lamp0/web/vhosts/www.nielsreijers.com/htdocs/user/pages/02.cv" | sftp sftp://6898308@sftp.dc2.gpaas.net
+echo "Now go to https://6898308.admin.dc2.gpaas.net/purge.php?all to purge the Varnish cache"
